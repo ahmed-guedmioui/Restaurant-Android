cc8d97aa63e3c164d797214aa94bdecae2c960b3
ahmed-guedmioui <ahmedgire01@gmail.com>
2024-11-15 12:39:33 +0100

    Created restaurant android app

---
+++ androidApp/src/main/java/com/ag_apps/restaurants/android/core/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ag_apps.restaurants.android.core
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ag_apps.restaurants.android.restaurant_list.RestaurantScreenCore
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RestaurantScreenCore()
+                }
+            }
+        }
+    }
+}
+
